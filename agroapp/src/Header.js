37e9b96a5de61d09062cb0c105375177a60282dd
JunArredodno <juan.e.arredondosaenz@gmail.com>
2022-04-28 08:25:26 -0500

    Primera_transicion

---
+++ agroapp/src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./header.css"
+
+
+function Header() {
+  return (
+    <header className="header">
+        <div className='header__menu'>
+            <h1 class="logo"> Agr@pp</h1>
+            <div className="header__nav">
+                <a class="menu__link select" href="index.html" >Inicio</a>
+                <a class="menu__link " href="#info">Productos</a>
+                <a class="menu__link " href="#contactenos">Contactenos</a>
+                <a class="menu__link " href="#nosotros" >Mi cuenta</a>
+                <a class="menu__link " href="#contactenos">Registrarse</a>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
