bc5542ce15e0a6100ae223b8ef48fa83c6361f65
JunArredodno <juan.e.arredondosaenz@gmail.com>
2022-04-28 14:00:30 -0500

    home

--- agroapp/src/Header.js
+++ agroapp/src/Header.js
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="header">
         <div className='header__menu'>
-            <h1 class="logo"> Agr@pp</h1>
+            <h1 class="logo"> Agr@ppWeb</h1>
             <div className="header__nav">
                 <a class="menu__link select" href="index.html" >Inicio</a>
                 <a class="menu__link " href="#info">Productos</a>
---
+++ agroapp/src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./home.css"
+
+function home() {
+  return (
+    <div className='home'>
+        <div className='home__banner'>
+            <h1>Agr@pp</h1>
+            <button className='boton'>Conocenos</button>
+        </div>
+        <div className='home__content'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default home
