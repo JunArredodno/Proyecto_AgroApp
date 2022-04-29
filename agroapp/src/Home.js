5cffe9e1838d051828249983950790b7228e8dda
sender_gonzalo <sender.molano@campusvirtual.aunarvillavicencio.edu.co>
2022-04-29 11:58:58 -0500

    ACTUALIZANDO CATEGORIAS

--- agroapp/src/Home.js
+++ agroapp/src/Home.js
@@ -55,6 +55,83 @@ function home() {
 
             </div>
         
+        
+            <div className='home__content__title'>
+                <h2>Frutas   </h2>
+            </div>
+            <div className='home__content__clusters'>
+                
+                    <a href='' className='imagen1'>
+                    <div >
+                        <h3>Lulo</h3>
+                        </div>
+                    </a>
+
+                    <a href='' className='imagen2'>
+                    <div >
+                        <h3>Mora</h3>
+                        </div>
+                    </a>
+
+                    <a href='' className='imagen3'>
+                    <div >
+                        <h3>Tomate de arbol</h3>
+                        </div>
+                    </a>
+
+            </div>
+      
+
+        
+            <div className='home__content__title'>
+                <h2>Legumbres</h2>
+            </div>
+            <div className='home__content__clusters'>
+                
+                    <a href='' className='imagen1'>
+                    <div >
+                        <h3>Frijol</h3>
+                        </div>
+                    </a>
+
+                    <a href='' className='imagen2'>
+                    <div >
+                        <h3>Arbeja</h3>
+                        </div>
+                    </a>
+
+                    <a href='' className='imagen3'>
+                    <div >
+                        <h3>Habichuela</h3>
+                        </div>
+                    </a>
+                    
+
+            </div>
+            
+        </div>
+        <a href="http://" class="product">VER MAS PRODUCTOS</a>
+
+        <div className='destacado'>
+                
+                    
+                    <div className='imagen10'>
+                        
+                        </div>
+                    
+
+                    <div className='descripcion1'>
+                    <h1 class="nom">Nombre del producto: </h1>
+                    <h1 class="agri">Nombre del agricultor: </h1>
+                    <h1 class="des">Decripcion del producto: </h1>
+                    
+                        </div>
+                   
+            </div>
+
+            <div className='home_contacto'>
+
+                
         </div>
     </div>
   )
